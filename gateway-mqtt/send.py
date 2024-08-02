e7b0bce3a4b433e7358c36e345a8d0b8207dee14
Erik Houge Mathisen <erik.houge.mathisen@tellu.no>
2024-08-02 15:12:02 +0200

    Updates, for pushing messages to research cloud (success!)

--- gateway-mqtt/send.py
+++ gateway-mqtt/send.py
@@ -1,18 +1,11 @@
-# Define connection parameters
-# connection_params = pika.ConnectionParameters(
-#     host='mqtt',  # Use the service name defined in Docker Compose
-#     port=5672,
-#     credentials=pika.PlainCredentials('lapin', 'lapin')
-# )
-
 import paho.mqtt.client as mqtt
 import time
 
 # Define the MQTT broker address and port
-broker_address = "mqtt"  # Use the service name defined in Docker Compose
-broker_port = 1883       # Default MQTT port
-username = "lapin"       # Username for authentication
-password = "lapin"       # Password for authentication
+broker_address = "3.254.120.13"  #IP address of remote server
+broker_port = 1885       # MQTT port
+username = "admin"       # Username for authentication
+password = "admin"       # Password for authentication
 topic = "gateway"
 message = "Hello, World!"
 
