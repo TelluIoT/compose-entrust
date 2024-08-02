6f8d2379b255c110c4ee6685fa3b4eb8dfc7f23f
Erik Houge Mathisen <erik.houge.mathisen@tellu.no>
2024-08-02 13:26:25 +0200

    Successfully sending to a predefined topic (queue+binding) in RabbitMQ admin

--- gateway-mqtt/send.py
+++ gateway-mqtt/send.py
@@ -1,30 +1,37 @@
-import pika
+# Define connection parameters
+# connection_params = pika.ConnectionParameters(
+#     host='mqtt',  # Use the service name defined in Docker Compose
+#     port=5672,
+#     credentials=pika.PlainCredentials('lapin', 'lapin')
+# )
+
+import paho.mqtt.client as mqtt
 import time
 
-# Define connection parameters
-connection_params = pika.ConnectionParameters(
-    host='mqtt',  # Use the service name defined in Docker Compose
-    port=5672,
-    credentials=pika.PlainCredentials('lapin', 'lapin')
-)
-
-# Establish connection
-connection = pika.BlockingConnection(connection_params)
-print("Connection established: ", connection)
-
-# Open a channel
-channel = connection.channel()
-print("Channel opened: ", channel)
-
-# Declare a queue
-channel.queue_declare(queue='rpm-measurements')
+# Define the MQTT broker address and port
+broker_address = "mqtt"  # Use the service name defined in Docker Compose
+broker_port = 1883       # Default MQTT port
+username = "lapin"       # Username for authentication
+password = "lapin"       # Password for authentication
+topic = "gateway"
+message = "Hello, World!"
 
 
+# Create a new MQTT client instance
+client = mqtt.Client()
 
-# Send a message
-channel.basic_publish(exchange='', routing_key='rpm-measurements', body='Hello, World!')
+# Set username and password
+client.username_pw_set(username, password)
 
-print(" [x] Sent 'Hello, World!'")
+# Connect to the broker
+client.connect(broker_address, broker_port, 60)
 
-# Close the connection
-connection.close()
+# Publish a message to the specified topic
+client.publish(topic, message)
+
+time.sleep(30)
+
+# Disconnect from the broker
+client.disconnect()
+
+print(f"Message '{message}' published to topic '{topic}'")
