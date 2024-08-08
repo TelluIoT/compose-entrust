7bcbe3576eac4b4752f31c0e370b9ad229f0939d
Maciej Zuziak <maciej.zuziak@tellu.com>
2024-08-08 15:44:38 +0200

    Changes includes password-based authentication

--- gateway-mqtt/send.py
+++ gateway-mqtt/send.py
@@ -2,13 +2,12 @@ import paho.mqtt.client as mqtt
 import time
 
 # Define the MQTT broker address and port
-broker_address = "3.254.120.13"  #IP address of remote server
-broker_port = 1885       # MQTT port
-username = "admin"       # Username for authentication
-password = "admin"       # Password for authentication
+broker_address = "mqtt"  #IP address of remote server
+broker_port = 1883       # MQTT port
+username = "CMD1"       # Username for authentication
+password = "user1"       # Password for authentication
 topic = "gateway"
-message = "Hello, World!"
-
+message = "This is a message from CMD1. Authorized"
 
 # Create a new MQTT client instance
 client = mqtt.Client()
@@ -19,12 +18,12 @@ client.username_pw_set(username, password)
 # Connect to the broker
 client.connect(broker_address, broker_port, 60)
 
+time.sleep(5)
+
 # Publish a message to the specified topic
 client.publish(topic, message)
 
-time.sleep(30)
-
-# Disconnect from the broker
+time.sleep(20)
+# # Disconnect from the broker
 client.disconnect()
-
 print(f"Message '{message}' published to topic '{topic}'")
